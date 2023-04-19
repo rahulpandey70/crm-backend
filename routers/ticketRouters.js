b95a82415decbf298d6d7c6ae34b476abeff62d2
rahulpandey70 <rahulpandey9103@gmail.com>
2023-04-19 08:21:28 +0530

    Endpoint for creating ticket

--- routers/ticketRouters.js
+++ routers/ticketRouters.js
@@ -1,7 +1,42 @@
+const { insertTicket } = require("../model/ticketModel/ticketModel");
+
 const router = require("express").Router();
 
-router.all("/", (req, res) => {
-	res.json({ msg: "hello from ticket router" });
+router.all("/", (req, res, next) => {
+	next();
+});
+
+router.post("/", async (req, res) => {
+	try {
+		const { subject, sender, message } = req.body;
+
+		const ticketObj = {
+			clientId: "643cfe028fa24b49263421fd",
+			subject,
+			conversations: [
+				{
+					sender,
+					message,
+				},
+			],
+		};
+
+		const result = await insertTicket(ticketObj);
+
+		if (result._id) {
+			return res.json({ status: "Success", messgae: "New Ticket created!" });
+		}
+
+		res.json({
+			status: "Error",
+			message: "Somthing is wrong, Try Again later!",
+		});
+	} catch (error) {
+		res.json({
+			status: "Error",
+			message: error.message,
+		});
+	}
 });
 
 module.exports = router;
